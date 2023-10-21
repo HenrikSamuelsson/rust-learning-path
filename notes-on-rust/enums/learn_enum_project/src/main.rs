adb6d511f8c8b9782815acc91946be47ac1ac74e
Henrik Samuelsson <henrik.samuelsson2@saabgroup.com>
2023-10-21 08:44:23 +0200

    Display the enum type of a chess piece.

--- notes-on-rust/enums/learn_enum_project/src/main.rs
+++ notes-on-rust/enums/learn_enum_project/src/main.rs
@@ -1,3 +1,6 @@
+use::core::fmt;
+
+#[derive(Debug, Clone, Copy)]
 enum ChessPiece {
     Pawn,
     Bishop,
@@ -7,7 +10,20 @@ enum ChessPiece {
     King,
 }
 
+impl fmt::Display for ChessPiece {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            ChessPiece::Pawn => write!(f, "pawn"),
+            ChessPiece::Bishop => write!(f, "bishop"),
+            ChessPiece::Knight => write!(f, "knight"),
+            ChessPiece::Rook => write!(f, "rook"),
+            ChessPiece::Queen => write!(f, "queen"),
+            ChessPiece::King => write!(f, "king"),
+        }
+    }
+}
+
 fn main() {
-    let paw_1 = ChessPiece::Pawn;
-    println!("Hello, world!");
+    let piece = ChessPiece::Pawn;
+    println!("Our chess piece is a {}", piece);
 }
